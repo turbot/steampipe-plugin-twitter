8e1c466eff1474ff71ce15f3cfba36540101d7cd
misraved <ved@turbot.com>
2023-12-12 17:20:55 +0530

    Update connection config variable definition to use the latest hcl syntax

--- twitter/connection_config.go
+++ twitter/connection_config.go
@@ -2,40 +2,18 @@ package twitter
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type twitterConfig struct {
 	// OAuth 2.0
-	BearerToken *string `cty:"bearer_token"`
+	BearerToken *string `hcl:"bearer_token"`
 	// OAuth 1.0
-	ConsumerKey    *string `cty:"consumer_key"`
-	ConsumerSecret *string `cty:"consumer_secret"`
-	AccessToken    *string `cty:"access_token"`
-	AccessSecret   *string `cty:"access_secret"`
+	ConsumerKey    *string `hcl:"consumer_key"`
+	ConsumerSecret *string `hcl:"consumer_secret"`
+	AccessToken    *string `hcl:"access_token"`
+	AccessSecret   *string `hcl:"access_secret"`
 	// Limits
-	MaxItemsPerQuery *int `cty:"max_items_per_query"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"bearer_token": {
-		Type: schema.TypeString,
-	},
-	"consumer_key": {
-		Type: schema.TypeString,
-	},
-	"consumer_secret": {
-		Type: schema.TypeString,
-	},
-	"access_token": {
-		Type: schema.TypeString,
-	},
-	"access_secret": {
-		Type: schema.TypeString,
-	},
-	"max_items_per_query": {
-		Type: schema.TypeInt,
-	},
+	MaxItemsPerQuery *int `hcl:"max_items_per_query"`
 }
 
 func ConfigInstance() interface{} {
